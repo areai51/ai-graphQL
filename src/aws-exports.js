dd1db07454cd363f2ca1c59800c018a15666a1e6
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2018-07-13 09:56:55 +0530

    structure change

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    'aws_appsync_graphqlEndpoint': 'https://awubwzqmz5alpknxo2ig4v6bwa.appsync-api.us-east-1.amazonaws.com/graphql',
+    'aws_appsync_region': 'us-east-1',
+    'aws_appsync_authenticationType': 'API_KEY',
+    'aws_appsync_apiKey': 'da2-ri4isbjadngetnsf33z3jugj5a'
+}
+
+export default awsmobile
