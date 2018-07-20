cc680f929fb1fde954ac184e41a13dcf4cbdf262
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2018-07-20 10:19:23 +0530

    sync

--- src/aws-export.js
+++
@@ -1,9 +0,0 @@
-const awsmobile = {
-    'aws_appsync_graphqlEndpoint': 'https://xxxxxx.appsync-api.us-east-1.amazonaws.com/graphql',
-      'aws_appsync_region': 'us-east-1',
-      'aws_appsync_authenticationType': 'API_KEY',
-      'aws_appsync_apiKey': 'da2-xxxxxxxxxxxxxxxxxxxxxxxxxx',
-      'aws_s3_url':'https://s3.ap-south-1.amazonaws.com/YOURBUCKETNAME/'
-  }
-  
-  export default awsmobile;
